1301aebf61c6c19d3251846d6d64d5a1c6b94795
Gami Projekt <gamiprojekt@gmail.com>
2025-10-24 21:43:18 +0000

    added frontend_web for gh-pages

---
+++ Config.js
@@ -0,0 +1,13 @@
+/******************************************************
+ * ⚙️ Zentrale Konfiguration
+ ******************************************************/
+const CONFIG = {
+  APP_NAME: 'HybridSheetApp',
+  SHEET_ID: '1f0OCgNZirx775kfc-F8giM3VfXz0gfW8LjYdd2TbAPA',
+  SHEET_SCHAUSTELLER: 'Schausteller' // ← Name des Tabs in deinem Sheet
+};
+
+function getConfig() {
+  return CONFIG;
+}
+
